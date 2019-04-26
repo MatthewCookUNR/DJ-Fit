795de5bdf854787f098d0ba772ef8b955ddd29ee
MatthewCookUNR <matthewthomascook@yahoo.com>
2019-04-25 19:06:31 -0700

    Successful email/password authentication

--- app/src/main/java/com/example/dj_fit/MainActivity.java
+++ app/src/main/java/com/example/dj_fit/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.dj_fit;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText emailText, passwordText;
+    private Button btnSignIn, btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +33,71 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignOut = findViewById(R.id.btnSignOut);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try
+                {
+                    String currentEmail = emailText.getText().toString();
+                    if(currentEmail.isEmpty())
+                    {
+                        throw new Exception();
+                    }
+
+                    String currentPass = passwordText.getText().toString();
+                    if(currentPass.isEmpty())
+                    {
+                        throw new Exception();
+                    }
+
+                    mAuth.signInWithEmailAndPassword(currentEmail, currentPass)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("Auth", "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(MainActivity.this, "Successfully signed in",
+                                                Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w("Auth", "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(MainActivity.this, "Please enter a email and password.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +105,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        onStart();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null)
+        {
+            Toast.makeText(MainActivity.this, "No one logged in", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
