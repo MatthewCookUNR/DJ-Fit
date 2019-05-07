43f0a6524f99fd2264116380028b0d0e0a53088b
MatthewCookUNR <matthewthomascook@yahoo.com>
2019-05-07 01:40:25 -0700

    Adding day to workout routine recreated dynamically

--- app/src/main/java/com/example/dj_fit/WorkoutOutline.java
+++ app/src/main/java/com/example/dj_fit/WorkoutOutline.java
@@ -1,9 +1,12 @@
 package com.example.dj_fit;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +24,6 @@ public class WorkoutOutline extends BaseActivity {
     private RelativeLayout container;
     private EditText hrEdit, restPeriodEdit, repRangeEdit;
     private Button btnAddDay, btnSaveOutline;
-    private int[] intArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +38,13 @@ public class WorkoutOutline extends BaseActivity {
         repRangeEdit = findViewById(R.id.repRangeEdit);
         btnSaveOutline = findViewById(R.id.btnWorkoutOutline);
         btnAddDay = findViewById(R.id.btnAddDay);
-        intArray = new int[5];
 
+        //Handles the on click function of adding a day to the workout outline
         btnAddDay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Creates Textview representing day of a particular workout (Mon-Sun)
                 TextView mText = new TextView(WorkoutOutline.this);
                 RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 params.topMargin = 85;
@@ -52,54 +56,78 @@ public class WorkoutOutline extends BaseActivity {
                 mText.setGravity(Gravity.CENTER);
                 mText.setLayoutParams(params);
                 container.addView(mText);
-                intArray[integer] = mText.getId();
                 integer++;
 
+                //Creates a table to outline workout
                 TableLayout newTable = new TableLayout(WorkoutOutline.this);
                 TableRow newRow1 = new TableRow(WorkoutOutline.this);
-
-                TextView exerText = new TextView(WorkoutOutline.this);
-                exerText.setGravity(Gravity.CENTER);
-                exerText.setTextSize(10);
-                exerText.setText("Exercises");
-                TableRow.LayoutParams paramEX1 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, .62f);
-                exerText.setLayoutParams(paramEX1);
-
-                TextView targetText = new TextView(WorkoutOutline.this);
-                targetText.setGravity(Gravity.CENTER);
-                targetText.setTextSize(10);
-                targetText.setText("Target");
-                TableRow.LayoutParams paramEX2 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, .20f);
-                targetText.setLayoutParams(paramEX2);
-
-                TextView minText = new TextView(WorkoutOutline.this);
-                minText.setGravity(Gravity.CENTER);
-                minText.setTextSize(10);
-                minText.setText("Min Weight");
-                TableRow.LayoutParams paramEX3 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, .09f);
-                minText.setLayoutParams(paramEX3);
-
-                TextView maxText = new TextView(WorkoutOutline.this);
-                maxText.setGravity(Gravity.CENTER);
-                maxText.setTextSize(10);
-                maxText.setText("Max Weight");
-                TableRow.LayoutParams paramEX4 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, .09f);
-                maxText.setLayoutParams(paramEX4);
-
-                EditText edit1 = new EditText(WorkoutOutline.this);
+                TableRow newRow2 = new TableRow(WorkoutOutline.this);
 
 
+                //Creates first row that defines what is in each column
+                TextView exerTitle = new TextView(WorkoutOutline.this);
+                exerTitle.setGravity(Gravity.CENTER);
+                exerTitle.setTextSize(10);
+                exerTitle.setText("Exercise");
+                TableRow.LayoutParams paramColumn1 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, .60f);
+                exerTitle.setLayoutParams(paramColumn1);
+
+                TextView targetTitle = new TextView(WorkoutOutline.this);
+                targetTitle.setGravity(Gravity.CENTER);
+                targetTitle.setTextSize(10);
+                targetTitle.setText("Target");
+                TableRow.LayoutParams paramColumn2 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT, .20f);
+                targetTitle.setLayoutParams(paramColumn2);
+
+                TextView minTitle = new TextView(WorkoutOutline.this);
+                minTitle.setGravity(Gravity.CENTER);
+                minTitle.setTextSize(10);
+                minTitle.setText("Min Weight");
+                TableRow.LayoutParams paramColumn3 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT, .1f);
+                minTitle.setLayoutParams(paramColumn3);
+
+                TextView maxTitle = new TextView(WorkoutOutline.this);
+                maxTitle.setGravity(Gravity.CENTER);
+                maxTitle.setTextSize(10);
+                maxTitle.setText("Max Weight");
+                TableRow.LayoutParams paramColumn4 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT, .1f);
+                maxTitle.setLayoutParams(paramColumn4);
+
+                //Create second row of table that shows what muscle group is targeted
+                TextView exerType = new TextView(WorkoutOutline.this);
+                exerType.setTextSize(18);
+                exerType.setBackgroundResource(R.drawable.edit_border);
+                exerType.setText("Chest");
+                exerType.setTypeface(Typeface.DEFAULT_BOLD);
+                TableRow.LayoutParams exerTypeParam = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f);
+                exerType.setLayoutParams(exerTypeParam);
+
+                //Sets TableLayout below Textview for the desired day
                 RelativeLayout.LayoutParams paramsR = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 paramsR.addRule(RelativeLayout.BELOW, 1);
                 paramsR.topMargin = 85;
                 TableLayout.LayoutParams params2 = new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 newTable.setLayoutParams(params2);
-                newRow1.addView(exerText);
-                newRow1.addView(targetText);
-                newRow1.addView(minText);
-                newRow1.addView(maxText);
 
+                //Adds elements of first row
+                newRow1.addView(exerTitle);
+                newRow1.addView(targetTitle);
+                newRow1.addView(minTitle);
+                newRow1.addView(maxTitle);
+
+                //Adds elements of second row
+                newRow2.addView(exerType);
+
+                //Add rows to the table
                 newTable.addView(newRow1);
+                newTable.addView(newRow2);
+                newTable.addView(createBaseRow(true, paramColumn1, paramColumn2, paramColumn3, paramColumn4));
+                newTable.addView(createBaseRow(false, paramColumn1, paramColumn2, paramColumn3, paramColumn4));
+                newTable.addView(createBaseRow(false, paramColumn1, paramColumn2, paramColumn3, paramColumn4));
+                newTable.addView(createBaseRow(false, paramColumn1, paramColumn2, paramColumn3, paramColumn4));
+                newTable.addView(createBaseRow(false, paramColumn1, paramColumn2, paramColumn3, paramColumn4));
+
+                //Create the table on screen
                 container.addView(newTable, paramsR);
 
             }
@@ -115,9 +143,65 @@ public class WorkoutOutline extends BaseActivity {
         });
     }
 
-    void addTable()
+    //Function returns a new row for the workout outline to be put in a table
+    TableRow createBaseRow(boolean warmUp, TableRow.LayoutParams paramColumn1, TableRow.LayoutParams paramColumn2,
+                           TableRow.LayoutParams paramColumn3, TableRow.LayoutParams paramColumn4)
     {
+        TableRow baseRow = new TableRow(WorkoutOutline.this);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
+        baseRow.setLayoutParams(params);
+
+        EditText exerEdit = new EditText(WorkoutOutline.this);
+        if(warmUp == true)
+        {
+            exerEdit.setHint("Exercise name \n(Warm up)");
+        }
+        else
+        {
+            exerEdit.setHint("Exercise name");
+        }
+        exerEdit.setTextSize(14);
+        exerEdit.setLines(2);
+        exerEdit.setSingleLine(false);
+        exerEdit.setWidth(0);
+        exerEdit.setFilters(new InputFilter[]{new InputFilter.LengthFilter(40)});
+        exerEdit.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+        exerEdit.setBackgroundResource(R.drawable.edit_border);
+        exerEdit.setLayoutParams(paramColumn1);
+
+        TextView viewTarget = new TextView(WorkoutOutline.this);
+        viewTarget.setText("View");
+        viewTarget.setTextSize(14);
+        viewTarget.isClickable();
+        viewTarget.setBackgroundResource(R.drawable.edit_border);
+        viewTarget.setGravity(Gravity.CENTER);
+        viewTarget.setLayoutParams(paramColumn2);
+
+        EditText minEdit = new EditText(WorkoutOutline.this);
+        minEdit.setHint("#");
+        minEdit.setTextSize(14);
+        minEdit.setGravity(Gravity.CENTER);
+        minEdit.setBackgroundResource(R.drawable.edit_border);
+        minEdit.setInputType(InputType.TYPE_CLASS_NUMBER);
+        minEdit.setFilters(new InputFilter[]{new InputFilter.LengthFilter(3)});
+        minEdit.setLayoutParams(paramColumn3);
+
+        EditText maxEdit = new EditText(WorkoutOutline.this);
+        maxEdit.setHint("#");
+        maxEdit.setTextSize(14);
+        maxEdit.setGravity(Gravity.CENTER);
+        maxEdit.setBackgroundResource(R.drawable.edit_border);
+        maxEdit.setInputType(InputType.TYPE_CLASS_NUMBER);
+        maxEdit.setFilters(new InputFilter[]{new InputFilter.LengthFilter(3)});
+        maxEdit.setLayoutParams(paramColumn3);
+
+        baseRow.addView(exerEdit);
+        baseRow.addView(viewTarget);
+        baseRow.addView(minEdit);
+        baseRow.addView(maxEdit);
+
+        return baseRow;
     }
 
 }
