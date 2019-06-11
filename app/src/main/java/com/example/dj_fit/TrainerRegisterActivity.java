01ba3db341c6ca519fdce951896839f6a3de08af
MatthewCookUNR <matthewthomascook@yahoo.com>
2019-06-10 21:02:39 -0700

    Upload image for trainer profile pic

--- app/src/main/java/com/example/dj_fit/TrainerRegisterActivity.java
+++ app/src/main/java/com/example/dj_fit/TrainerRegisterActivity.java
@@ -1,23 +1,38 @@
 package com.example.dj_fit;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class TrainerRegisterActivity extends BaseActivity
 {
     private static final int RESULT_LOAD_IMAGE = 1;
-    ImageView imageToUpload;
+    Uri imageToUpload;
     EditText experienceEdit, employmentEdit, aboutYouEdit;
-    Button btnUploadImage;
+    Button btnUploadImage, btnBecomeTrainer;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mDatabase;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +45,12 @@ public class TrainerRegisterActivity extends BaseActivity
         employmentEdit = findViewById(R.id.employmentEdit);
         aboutYouEdit = findViewById(R.id.aboutYouEdit);
         btnUploadImage = findViewById(R.id.btnUploadImage);
-        imageToUpload = new ImageView(this);
+        btnBecomeTrainer = findViewById(R.id.btnBecomeTrainer);
+        imageToUpload = null;
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseFirestore.getInstance();
+        mStorageRef= FirebaseStorage.getInstance().getReference("trainerPics");
 
         btnUploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +59,14 @@ public class TrainerRegisterActivity extends BaseActivity
                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
             }
         });
+
+        btnBecomeTrainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
     }
 
     @Override
@@ -46,8 +74,46 @@ public class TrainerRegisterActivity extends BaseActivity
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK)
         {
-            Uri selectedImage = data.getData();
-            imageToUpload.setImageURI(selectedImage);
+            imageToUpload = data.getData();
+        }
+    }
+
+    private String getFileExtension(Uri uri)
+    {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage()
+    {
+        if(imageToUpload != null)
+        {
+            StorageReference fileRef = mStorageRef.child(System.currentTimeMillis() + "." +
+            getFileExtension(imageToUpload));
+
+            fileRef.putFile(imageToUpload).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Toast.makeText(TrainerRegisterActivity.this, "File upload success", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(TrainerRegisterActivity.this, "File upload failed", Toast.LENGTH_SHORT).show();
+
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
         }
     }
 }
